c82a1112ce0847706161bf7b493d4e698e9a5995
Erin Orstrom <eorstrom2010@gmail.com>
2016-01-12 19:30:15 -0600

    Many files added

---
+++ the-static-web/javascript-exercises/boy-bands-and-vegetables/boybands.js
@@ -0,0 +1,43 @@
+// Loop through the two arrays provided (bands and vegetables) and output each element in the arrays into their corresponding HTML <div> element. Ensure that each item is in a block element (e.g. li, div, p. etc...)
+
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+// The number of loops to perform (what if the array changes?)
+var loopCount = 5;
+
+// Keep track of which band we're on in the loop
+var currentBand;
+
+// Keep track of which veggie we're on in the loop
+var currentVeggie;
+
+// Get a reference to the appropriate DOM element for bands
+var bandElement = document.getElementById("boy-bands");
+
+// Get a reference to the appropriate DOM element for vegetables
+var veggieElement = document.getElementById("vegetables");
+
+// Start looping
+for (var i = 0; i < loopCount; i += 1) {
+
+  // Add the band names into the correct <div>
+  currentBand = bands[i];
+    if (i < bands.length - 1) {
+        currentBand 
+    } else {
+        currentBand
+    };
+    bandElement.innerHTML += "<li>" + currentBand + "</li>";
+    console.log("current band:", currentBand);
+
+  // Add the veggie names into the correct <div>
+  currentVeggie = vegetables[i];
+    if (i < vegetables.length - 1) {
+        currentVeggie
+    } else {
+        currentVeggie
+    };
+    veggieElement.innerHTML += "<li>" + currentVeggie + "</li>";
+    console.log("current veggie:", currentVeggie);
+}
---
+++ the-static-web/javascript-exercises/reindeer/reindeer.js
@@ -0,0 +1,10 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+var coloredReindeer = [ ];
+
+for (var i = 0; i < reindeer.length; i++) {
+    coloredReindeer.push(" " + colors[i] + " " + reindeer[i]);
+};
+
+var hohohoElement = document.getElementById("reindeer");
+hohohoElement.innerHTML = (coloredReindeer).join(", ");
