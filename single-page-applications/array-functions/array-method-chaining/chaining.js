445313d4d41eeed380971653b2dad201328c940f
Erin Orstrom <eorstrom2010@gmail.com>
2016-02-26 14:23:47 -0600

    completed each part of the exercise, but have been unable to successfully get the methods chained together in one line of JS

--- single-page-applications/array-functions/array-method-chaining/chaining.js
+++ single-page-applications/array-functions/array-method-chaining/chaining.js
@@ -1,20 +1,47 @@
 /*
 Using one single line of JavaScript code, complete the following tasks on the array of integers below.
-
-Sort the numbers in descending order (10, 9, 8, 7, etc).
-Remove any integers greater than 19.
-Multiply each remaining number by 1.5 and then substract 1.
-Then output (either in the DOM or the console) the sum of all the resulting numbers.
 */
 
+// starting integer array
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+var newIntArray = [];
+var mathDoneArray = [];
 
-function filterIntegers(element, index, array) {
-    if (index > 19) {
-        var remove = index.unshift();
-    }
-    console.log(remove);
-}
 
-var stuffDone = integers.sort(function(first, second) { return second - first; }).filter(filterIntegers);
-console.log("stuffDone", stuffDone);
+// Sort the numbers in descending order (10, 9, 8, 7, etc).
+    var sortIntegers = integers.sort(function(first, second) { return second - first; });
+    console.log("sortIntegers", sortIntegers);
+
+// Remove any integers greater than 19.
+    function filterIntegers(value) {
+        if (value <= 19) {
+            return value;
+        }
+    };
+
+var filteredIntegers = sortIntegers.filter(filterIntegers);
+console.log("filteredIntegers", filteredIntegers);
+
+// Multiply each remaining number by 1.5 and then subtract 1.
+
+    function mathIntegers(arr) {
+        arr.forEach(function(currentItem){
+            mathDoneArray.push((currentItem * 1.5) - 1);
+        });
+    };
+
+mathIntegers(filteredIntegers);
+console.log("mathDoneArray", mathDoneArray);
+
+var total = mathDoneArray.reduce(function(a, b) { return a + b; }, 0);
+
+// Then output (either in the DOM or the console) the sum of all the resulting numbers. 
+console.log("sum", total);
+document.write(total);
+
+
+
+    // still doesn't work
+    // integers = integers.sort(function(first, second) { return second - first; }).filter(filterIntegers).forEach(mathIntegers(integers)).reduce(function(a, b) { return a + b; }, 0);
+
+    // console.log("integers", integers);
